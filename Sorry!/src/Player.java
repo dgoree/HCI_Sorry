a54ebdd14e22bbf6ce44c599d190a37831eea433
Daniel Goree <daniel.botball@gmail.com>
2017-03-27 17:15:31 -0500

    Created Player Class

---
+++ Sorry!/src/Player.java
@@ -0,0 +1,29 @@
+
+public class Player {
+	private Color color;
+	private Token[] tokens;
+	
+	public Player(Color color, Token[] tokens) {
+		this.color = color;
+		this.tokens = tokens;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public Token[] getTokens() {
+		return tokens;
+	}
+
+	public void setTokens(Token[] tokens) {
+		this.tokens = tokens;
+	}
+	
+	
+
+}
