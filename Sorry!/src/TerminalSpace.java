d77f5b713b48bbc2a55a188764051312d4633326
Daniel Goree <daniel.botball@gmail.com>
2017-03-27 17:17:21 -0500

    Refactored TerminalSpace enum->TerminalType, SafeSpace->TerminalSpace

--- Sorry!/src/TerminalSpace.java
+++ Sorry!/src/TerminalSpace.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+import java.util.UUID;
 
-public enum TerminalSpace {
-	START, HOME
+
+public class TerminalSpace extends Space{
+
+	private ArrayList<Token> tokens;
+	private TerminalType type;
+
+	//fully customizable
+	public TerminalSpace(Space previous, Space next, Space slideTo, Space safeNext, Color color, boolean isSafe, TerminalType type) {
+		super(previous, next, slideTo, safeNext, color, isSafe);
+		this.tokens = new ArrayList<Token>();
+		this.type = type;
+	}
+	
+	public boolean add(Token token) {
+		return this.tokens.add(token);
+	}
+
+	public ArrayList<Token> getTokens() {
+		return tokens;
+	}
+
+	public void setTokens(ArrayList<Token> tokens) {
+		this.tokens = tokens;
+	}
+
+	public TerminalType getType() {
+		return type;
+	}
+
+	public void setType(TerminalType type) {
+		this.type = type;
+	}
+	
+	@Override
+	public boolean isSafe() {
+		return true;
+	}
+	
+	
+	
+
 }
